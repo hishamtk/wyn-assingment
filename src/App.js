84a9db0829a6ed94029e8fe9db4f424da897fbaf
hishamtk <tkhisham007@gmail.com>
2021-11-01 16:03:43 +0530

    audio analyser complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 
 
-import React from 'react';
+import React, { useState } from 'react';
+import AudioAnalyser from './Components/AudioAnalyser';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
 
 function App() {
+
+  const [audio ,setAudio] = useState(null)
+
+  const  getMicrophone =async () =>{
+    const audio  = await navigator.mediaDevices.getUserMedia({
+      audio:true,
+      video:false
+    })
+    setAudio(audio)
+  }
+
+  const stopMicrophone = () =>{
+    audio.getTracks().forEach(track=>track.stop())
+    setAudio(null)
+  }
+
+  const toogleMicrophone = () =>{
+    if(audio){
+      stopMicrophone();
+    }else{
+      getMicrophone()
+    }
+  }
 
   return (
     <>
     <Header />
     <main className="App">
         <div className="controls">
-          <button>Get microphone input</button>
+          <button onClick={toogleMicrophone}>{audio?'Stop microphone':'Get microphone input'}</button>
         </div>
+        {audio?<AudioAnalyser audio={audio} />:""}
       </main>
 
       <Footer />
